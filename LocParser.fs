94843843bd9e8f4908a2f56ad59e5504d9376b2e
ffamoss <zikyrat@gmail.com>
2018-03-20 16:12:12 +0300

    init

---
+++ LocParser.fs
@@ -0,0 +1,183 @@
+﻿namespace Loc
+module LocParser =     
+    open FParsec
+    open System
+    open System.Collections.Generic
+    open System.IO
+    
+    type Id = string
+    
+    type Value =
+        | Literal of string
+        | KeyRef of Id
+        | Param of Id
+        | ParamPluralForm of Id * string list
+    
+    type Loc = | Data of Id * Value list
+               | Comment of string
+    
+    let skipSpaces: Parser<unit,'u> =
+        fun stream ->
+            stream.Skip(' ') |> ignore
+            Reply(())
+    
+    let ws = spaces
+    
+    let str_ws s = pstring s .>> ws
+    
+    let identifier : Parser<string,unit> =
+        let isIdentifierFirstChar c = isLetter c || c = '_'
+        let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+    
+        many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"  .>> ws 
+    
+    
+    let stringLiteral : Parser<string,unit> =
+        let normalChar = satisfy (fun c -> c <> '\\' && c <> ''')
+        let unescape c = match c with
+                         | 'n' -> '\n'
+                         | 'r' -> '\r'
+                         | 't' -> '\t'
+                         | c   -> c
+        let escapedChar = pstring "\\" >>. (anyOf "\\nrt'\"" |>> unescape)
+        
+        between (pstring "'") (pstring "'")
+                (manyChars (normalChar <|> escapedChar))
+    
+    let parameterKey : Parser<string,unit> =    
+        pstring "$" >>. identifier  .>> ws <?> "paramKey"
+    
+    let parameterKeyPluralForm : Parser<(string*string list),unit> =
+        tuple2 
+            (str_ws "{" >>. parameterKey) 
+            (str_ws ":" >>. (sepBy (stringLiteral .>> ws) (str_ws "," )) .>> ws .>> str_ws "}" ) <?> "plural form"
+    
+    let vparam = parameterKey |>> Value.Param    
+    let vparamPluralForm = parameterKeyPluralForm |>> Value.ParamPluralForm    
+    let vkeyRef = identifier |>> Value.KeyRef
+    let vliteral = stringLiteral |>> Value.Literal
+    
+    let value : Parser<Value list,unit> =
+        sepBy1 ( (vliteral .>> ws ) <|> vkeyRef <|> vparam <|> vparamPluralForm ) (skipSpaces >>.  str_ws "+")
+        
+        
+    let loc : Parser<(string*Value list),unit> =
+        tuple2 (ws >>. identifier .>> ws) ( str_ws "=" >>. value .>> ws )
+    
+    let locData = loc |>> Loc.Data
+    
+    let comment : Parser<string,unit> =
+        let commentStart c = c = ';'
+        let allExceptNewLine c =  c <> '\n'
+    
+        many1Satisfy2L commentStart allExceptNewLine "comment" .>> ws 
+    
+    let locComment =
+            comment |>> Loc.Comment
+    
+    let locFile : Parser<Loc list, unit> =
+        many (locComment <|> locData)
+        
+    
+    let pluralFormRules = dict [
+                            ("fa", []);
+                            ("id", []);
+                            ("ja", []);
+                            ("kk", []);
+                            ("ko", []);
+                            ("zh", []);
+                            ("tr", [fun n -> n < 2]);
+                            ("vi", []);
+                            ("th", []);
+                            
+                            ("bg", [fun n -> n = 1]);
+                            ("da", [fun n -> n = 1]);
+                            ("de", [fun n -> n = 1]);
+                            ("el", [fun n -> n = 1]);
+                            ("en", [fun n -> n = 1]);
+                            ("es", [fun n -> n = 1]);
+                            ("et", [fun n -> n = 1]);
+                            ("fi", [fun n -> n = 1]);
+                            ("hu", [fun n -> n = 1]);
+                            ("it", [fun n -> n = 1]);
+                            ("nl", [fun n -> n = 1]);
+                            ("no", [fun n -> n = 1]);
+                            ("nb", [fun n -> n = 1]);
+                            ("pt", [fun n -> n = 1]);
+                            ("sq", [fun n -> n = 1]);
+                            ("sv", [fun n -> n = 1]);
+                            ("ar", [fun n -> n = 0;
+                                    fun n -> n = 1;
+                                    fun n -> n = 2;
+                                    fun n -> (n % 100 >= 3) && (n % 100 <= 10);
+                                    fun n -> (n % 100 >= 11) && (n % 100 <= 99)]);
+                            ("fr",    [fun n -> n < 2]);
+                            ("pt-BR", [fun n -> n < 2]);    
+                            ("cs", [fun n -> n = 1;
+                                    fun n -> n >= 2 && n <= 4]);
+                            ("ga", [fun n -> n = 1;
+                                    fun n -> n = 2;
+                                    fun n -> n % 10 >= 3 && n % 10 <= 6;
+                                    fun n -> n % 10 >= 7 && n % 10 <= 9]);
+    
+                            ("lt", [fun n -> (n % 10 = 1) && (n % 100 <> 11);
+                                    fun n -> (n % 10 = 0) || (n % 100 >= 11 && n % 100 <= 19)]);
+                            
+                            ("lv", [fun n -> (n % 10 = 1) && (n % 100 <> 11);
+                                    fun n -> (n % 10 = 0) || (n % 100 >= 11 && n % 100 <= 19)]);
+                            
+                            ("mk", [fun n -> n = 1]);
+                            
+                            ("pl", [fun n -> n = 1;
+                                    fun n -> (n % 10 >= 2) && (n % 10 <= 4) && (n % 100 < 10  || n % 100 > 20)]);
+    
+                            ("ro", [fun n -> n = 1;
+                                    fun n -> n = 0 || (n % 100 > 0 && n % 100 < 20)]);
+    
+                            ("ru", [fun n -> n % 10 = 1 && n % 100 <> 11;
+                                    fun n -> n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 > 20)]);
+    
+                            ("uk", [fun n -> n % 10 = 1 && n % 100 <> 11;
+                                    fun n -> n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 > 20)]);
+                             
+                            ("sr", [fun n -> (n % 10 = 1) && (n % 100 <> 11);
+                                    fun n -> (n % 10 >= 2) && (n % 10 <= 4) && (n % 100 < 10 || n % 100 >= 20)]);
+                            
+                            ("sl", [fun n -> n = 1;
+                                    fun n -> n % 100 = 2;
+                                    fun n -> (n % 100 = 3) || (n % 100 = 4)])
+        ]
+    
+    
+    let paramResolver key culture (paramValueGetter : string -> Object) (pluralforms : string list) =
+        match paramValueGetter key with
+        | :? int as v  when pluralforms.Length > 0 ->                
+                    let form = match pluralforms with
+                               | [] -> ""
+                               | _ -> match pluralFormRules.TryGetValue culture  with
+                                      | true, rules -> pluralforms.[rules |> Seq.takeWhile (fun rule -> not (rule v)) |> Seq.length]
+                                      | _ -> ""
+                    Value.Literal (form)
+        | v -> Value.Literal (v.ToString())
+        
+    let keyResolver key (existLocData: IDictionary<string,Value list>)  =
+        match existLocData.TryGetValue key with
+        | true, value -> value
+        | _ -> failwithf "KeyRef %s not found" key
+    
+    let evalLocValue locValue culture paramValueGetter (existLocData: IDictionary<string,Value list>) =
+       let rec resolve v acc=
+            match v with
+            |h::xs -> match h with
+                      |Value.Literal x -> resolve xs (x::acc)
+                      |Value.Param x -> resolve ((paramResolver x culture paramValueGetter [])::xs) acc
+                      |Value.KeyRef k -> resolve ((keyResolver k existLocData)@xs) acc
+                      |Value.ParamPluralForm(key, lst) -> resolve ((paramResolver key culture paramValueGetter lst)::xs) acc
+            |[] -> acc  
+    
+       resolve locValue [] |> List.rev |> List.reduce (+)
+    
+    
+    let loadLocFile (fileStream : Stream) (encoding:  System.Text.Encoding)  =
+        runParserOnStream locFile () "" fileStream encoding
+         
---
+++ LocTests.fs
@@ -0,0 +1,195 @@
+﻿namespace Loc
+module LocTests = 
+    
+    open LocParser
+    
+    open FsUnit.Xunit
+    open Xunit
+    open FParsec
+    open System
+    open System.Collections.Generic
+    open System.IO
+    open System.Text
+    
+    
+    let testParser p str =
+        match run p str with
+        | Success(result, _, _)   -> Some result
+        | Failure(_, _, _) -> None
+    
+    [<Fact>]
+    let ``Quotated string must be Literal`` () =
+        let str = "'abc'"
+        let expected = Some(Value.Literal "abc")
+        testParser vliteral str |> should equal expected
+    
+    [<Fact>]
+    let ``Quotated string with escaped symbols must be Literal`` () =
+        let expectedStr = "a b s ' sas 123 __ !@#$%^ \n 02980"
+        let str = "'a b s \\' sas 123 __ !@#$%^ \\n 02980'"
+        let expected = Some(Value.Literal expectedStr)
+        testParser vliteral str |> should equal expected
+    
+    [<Fact>]
+    let ``Not quotated string is not Literal`` () =
+        let str = "'abc"
+        testParser vliteral str |> should equal None
+    
+    [<Fact>]
+    let ``DollarSign names is Param`` () =
+        let str = "$abc"
+        let expected = Some(Value.Param "abc")
+        testParser vparam str |> should equal expected
+    
+    [<Fact>]
+    let ``Without dollarSign names is not Param`` () =
+        let str = "abc"
+        testParser vparam str |> should equal None
+    
+    [<Fact>]
+    let ``DollarSign names with plural forms must be ParamPluralForm`` () =
+        let str = "{$abc: 'p1', 'p2'}"
+        let expected = Some(Value.ParamPluralForm("abc",["p1";"p2"]))
+        testParser vparamPluralForm str |> should equal expected
+    
+    [<Fact>]
+    let ``Without dollarSign names with plural forms Is't ParamPluralForm`` () =
+        let str = "{abc: 'p1', 'p2'}"
+        testParser vparam str |> should equal None
+    
+    [<Fact>]
+    let ``Without quots and with dollarSign names with plural forms Is't ParamPluralForm`` () =
+        let str = "{$abc: p1, 'p2'}"
+        testParser vparam str |> should equal None
+    
+    [<Fact>]
+    let ``Literal must be Value`` () =
+        let str = "'abc'"
+        let expected = Some([Value.Literal "abc"])
+        testParser value str |> should equal expected
+    
+    [<Fact>]
+    let ``Param must be Value`` () =
+        let str = "$abc"
+        let expected = Some([Value.Param "abc"])
+        testParser value str |> should equal expected
+    
+    [<Fact>]
+    let ``ParamPluralForm  must be Value`` () =
+        let str = "{$abc: 'p1', 'p2'}"
+        let expected = Some([Value.ParamPluralForm("abc",["p1";"p2"])])
+        testParser value str |> should equal expected
+    
+    [<Fact>]
+    let ``KeyRef must be Value`` () =
+        let str = "abc"
+        let expected = Some([Value.KeyRef "abc"])
+        testParser value str |> should equal expected
+    
+    [<Fact>]
+    let ``Literal + Param + ParamPluralForm + KeyRef must be Value`` () =
+        let str = "'literal' + $param + {$param: 'p1', 'p2'} + keyref"
+        let expected =Some [Literal "literal"; Param "param"; ParamPluralForm ("param",["p1"; "p2"]); KeyRef "keyref"]
+        testParser value str |> should equal expected
+    
+    [<Fact>]
+    let ``Id equal value must be LocData`` () =
+        let str = "a = 'abc'"
+        let expected = Some(Data("a",[Value.Literal "abc"]))
+        testParser locData str |> should equal expected
+    
+    [<Fact>]
+    let ``Id empty value Isn't LocData`` () =
+        let str = "a  'abc'"
+        testParser locData str |> should equal None
+    
+    [<Fact>]
+    let ``Comment is LocData`` () =
+        let str = "; comment"
+        let expected = Some( Loc.Comment "; comment")
+        testParser locComment str |> should equal expected
+    
+    [<Fact>]
+    let ``Eval Literal LocData`` () =
+        let data = [Value.Literal "test"]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> v :> Object) emptyDict |> should equal "test" 
+        
+    [<Fact>]
+    let ``Eval Param LocData`` () =
+        let data = [Value.Param "param"]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> (v+"1") :> Object) emptyDict |> should equal "param1" 
+    
+    [<Fact>]
+    let ``Eval ParamPluralForm select p1 LocData`` () =
+        let data = [ParamPluralForm ("param",["p1"; "p2"; "p3"])]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> 1 :> Object) emptyDict |> should equal "p1" 
+    
+    [<Fact>]
+    let ``Eval ParamPluralForm select p2 LocData`` () =
+        let data = [ParamPluralForm ("param",["p1"; "p2"; "p3"])]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> 2 :> Object) emptyDict |> should equal "p2" 
+    
+    [<Fact>]
+    let ``Eval ParamPluralForm select p3 LocData`` () =
+        let data = [ParamPluralForm ("param",["p1"; "p2"; "p3"])]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> 10 :> Object) emptyDict |> should equal "p3" 
+    
+    [<Fact>]
+    let ``Eval KeyRef LocData`` () =
+        let data = [Value.KeyRef "key1"]
+        let emptyDict : IDictionary<string,Value list> = dict[("key1", [Value.Literal "Key1Data"])]
+        evalLocValue data "ru" (fun v -> v :> Object) emptyDict |> should equal "Key1Data" 
+    
+    [<Fact>]
+    let ``Eval Literals LocData must be in correct order`` () =
+        let data = [Value.Literal "test1"; Value.Literal "test2"; Value.Literal "test3"]
+        let emptyDict : IDictionary<string,Value list> = dict[]
+        evalLocValue data "ru" (fun v -> v :> Object) emptyDict |> should equal "test1test2test3" 
+    
+    [<Fact>]
+    let ``Eval nested KeyRef LocData`` () =
+        let data = [Value.KeyRef "key1"]
+        let emptyDict : IDictionary<string,Value list> = dict[("key1", [Value.KeyRef "Key2"]); ("Key2", [Value.Literal "Data"])]
+        evalLocValue data "ru" (fun v -> v :> Object) emptyDict |> should equal "Data" 
+    
+    
+    [<Fact>]
+    let ``Loc file is LocData list`` () =
+        let str = @"; comment
+        key1 = 'data1'
+        key2 = 'data2' + $param"
+        let expected =Some( [Comment "; comment"; 
+                             Data ("key1",[Literal "data1"]); 
+                             Data ("key2",[Literal "data2"; Param "param"])])
+        testParser locFile str |> should equal expected
+    
+    
+    [<Fact(Skip = "Fill correct path and keys")>]
+    let ``Real loc file is LocData list`` () =
+        let path = @"ru\locs\Common.txt"
+        use stream = File.OpenRead(path) 
+        
+        let parserResult = match loadLocFile stream Encoding.UTF8  with
+                            | Success(result, _, _)   -> Some result
+                            | Failure(_, _, _) -> None
+        
+        do (parserResult.IsSome |> should equal true)
+    
+        let locDataMap = parserResult.Value
+                        |> List.filter (fun v -> match v with
+                                                    | Loc.Comment x -> false
+                                                    | _ -> true) 
+                        |> List.map (fun v -> match v with 
+                                                |Loc.Data(key,value) -> (key, value)
+                                                |_ -> failwith "unexpected")
+                        |> dict
+        
+        evalLocValue locDataMap.["Key1"] "ru"  (fun v -> v :> Object) locDataMap |> should equal "Fill correct data for Key1"
+    
+        evalLocValue locDataMap.["Key2"] "ru"  (fun v -> 1 :> Object) locDataMap |> should equal "Fill correct data for Key2"
+        
